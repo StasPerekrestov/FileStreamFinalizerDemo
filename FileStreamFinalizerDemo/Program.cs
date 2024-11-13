fb511379528287bc16bca6da8edc60b8dc9baf41
Stanislav Perekrestov <sperekrestov@dmvdesk.com>
2024-11-13 12:12:07 -0800

    makes the test more representative, according to jkotas comment

--- FileStreamFinalizerDemo/Program.cs
+++ FileStreamFinalizerDemo/Program.cs
@@ -1,22 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.CompilerServices;
+
 Foo();
+GC.Collect();
+GC.Collect();
+GC.Collect();
+GC.WaitForPendingFinalizers();
+GC.WaitForPendingFinalizers();
+GC.WaitForPendingFinalizers();
+GC.WaitForPendingFinalizers();
+Console.WriteLine("Done");
 
-
+[MethodImpl(MethodImplOptions.NoInlining)]
 static void Foo()
 {
-    using (new DerivedFileStream("FileStreamFinalizerDemo.csproj")) {}
-    new DerivedFileStream("FileStreamFinalizerDemo.csproj");
+    using (new DerivedFileStream("FileStreamFinalizerDemo.csproj"))
+    {
+    }
 
-    
-    GC.Collect();
-    GC.Collect();
-    GC.Collect();
-    GC.WaitForPendingFinalizers();
-    GC.WaitForPendingFinalizers();
-    GC.WaitForPendingFinalizers();
-    GC.WaitForPendingFinalizers();
-    Console.WriteLine("Done");
+    new DerivedFileStream("FileStreamFinalizerDemo.csproj");
 }
 
 internal sealed class DerivedFileStream : FileStream
